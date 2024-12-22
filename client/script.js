ee60aec2266b276e996954b9a76fe4a57629cff8
KattenH <h23kathe@du.se>
2024-12-22 15:16:47 +0100

    Uppdateringar

--- client/script.js
+++ client/script.js
@@ -2,13 +2,14 @@ const url = "http://localhost:3000/users";
 
 fetch(url)
     .then((response) => {
-        console.log(response);
         return response.json(); // Översätt svaret till JSON
     })
+
     .then((users) => {
+        console.log(users);
         // Skapa ul-elementet
         const ul = document.createElement('ul');
-
+    
         // Loopa igenom användarna och skapa li-element för varje användare
         users.forEach(user => {
             const li = document.createElement('li'); // Skapa ett li-element
@@ -16,8 +17,8 @@ fetch(url)
             // Skapa innehållet för varje li-element
             li.innerHTML = `Förnamn: ${user.firstName} // Efternamn: ${user.lastName} // Användarnamn: ${user.username}`; // Skriv ut användarens namn och användarnamn
 
-
             li.style.backgroundColor = user.color; // Sätt bakgrundsfärgen från användarens color-egenskap
+            
             ul.appendChild(li); // Lägg till li-elementet i ul-elementet
         });
 
